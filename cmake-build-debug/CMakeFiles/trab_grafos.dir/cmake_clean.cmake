ab7b549d049316af6f253b6497f5320dd7286da8
Thassya <thassya@ice.ufjf.br>
2018-04-16 09:49:00 -0300

    CLion Project

---
+++ cmake-build-debug/CMakeFiles/trab_grafos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trab_grafos.dir/main.cpp.o"
+  "trab_grafos.pdb"
+  "trab_grafos"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trab_grafos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
